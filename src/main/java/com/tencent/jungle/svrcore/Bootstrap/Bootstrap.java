85b8073b5306addfa93eb8ae81ad44c91fde4d25
ewanzhao(赵雨) <ewanzhao@tencent.com>
2018-04-08 20:07:30 +0800

    add file

--- src/main/java/com/tencent/jungle/svrcore/Bootstrap/Bootstrap.java
+++ src/main/java/com/tencent/jungle/svrcore/Bootstrap/Bootstrap.java
@@ -1,8 +1,34 @@
 package com.tencent.jungle.svrcore.Bootstrap;
 
+import com.tencent.jungle.svrcore.WorkerService;
+import com.tencent.jungle.svrcore.client.DefaultTimeoutManager;
+import com.tencent.jungle.svrcore.client.TimeoutManager;
+import com.tencent.jungle.svrcore.ws.ThreadPoolWorkerService;
+import org.apache.commons.configuration.PropertiesConfiguration;
+
+import java.io.File;
+
 public class Bootstrap {
     public static void main(String[] args) {
         System.out.println("hello,world");
+        //加载配置
+        PropertiesConfiguration tmp = new PropertiesConfiguration();
+        File cFile = new File("../conf/jungle.properties");
+        if (cFile.exists() && cFile.isFile()){
+            try {
+                tmp.load(cFile);
+            } catch (Exception e) {
+                throw new RuntimeException(e);
+            }
+        }
+
+        // workService --> 目前使用ThreadPoolWorkerService
+        //后续引入kilim的线程池
+        Class<? extends WorkerService> workerServiceClazz = ThreadPoolWorkerService.class;
+
+        //使用默认的超时管理器--client 发包
+        Class<? extends TimeoutManager> clazz = DefaultTimeoutManager.class;
+
         //监听端口
 
         //分发请求
---
+++ src/main/java/com/tencent/jungle/svrcore/client/InaccurateTimeoutManager.java
@@ -0,0 +1,84 @@
+package com.tencent.jungle.svrcore.client;
+
+import com.google.inject.Inject;
+import com.google.inject.Singleton;
+import io.netty.util.HashedWheelTimer;
+import io.netty.util.Timeout;
+import io.netty.util.TimerTask;
+import org.apache.commons.configuration.Configuration;
+import org.apache.commons.lang.NotImplementedException;
+
+import java.io.Closeable;
+import java.io.IOException;
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.Future;
+import java.util.concurrent.TimeUnit;
+import java.util.concurrent.TimeoutException;
+
+@Singleton
+public class InaccurateTimeoutManager implements TimeoutManager, Closeable {
+	final HashedWheelTimer timer;
+	
+	public InaccurateTimeoutManager(int tickms) {
+		timer = new HashedWheelTimer(tickms, TimeUnit.MILLISECONDS);
+		timer.start();
+	}
+	
+	@Inject
+	public InaccurateTimeoutManager(Configuration configs) {
+		this(configs.getInt("server.client.timeout_manager.tickms", 50));
+	}
+
+	@Override
+	public Future<?> watch(final Runnable task, long timeout) {
+		FutureAdapter adapter = new FutureAdapter(task);
+		adapter.wrap = timer.newTimeout(adapter, timeout, TimeUnit.MILLISECONDS);
+		return adapter;
+	}
+
+	@Override
+	public void close() throws IOException {
+		timer.stop();
+	}
+	
+	static class FutureAdapter<T> implements Future<T>, TimerTask{
+		Timeout wrap;
+		final Runnable task;
+		
+		FutureAdapter(Runnable task){
+			this.task = task;
+		}
+
+		@Override
+		public boolean cancel(boolean mayInterruptIfRunning) {
+			return wrap.cancel();
+		}
+
+		@Override
+		public boolean isCancelled() {
+			return wrap.isCancelled();
+		}
+
+		@Override
+		public boolean isDone() {
+			return wrap.isCancelled();
+		}
+
+		@Override
+		public T get() throws InterruptedException, ExecutionException {
+			throw new NotImplementedException();
+		}
+
+		@Override
+		public T get(long timeout, TimeUnit unit) throws InterruptedException,
+				ExecutionException, TimeoutException {
+			throw new NotImplementedException();
+		}
+
+		@Override
+		public void run(Timeout timeout) throws Exception {
+			task.run();
+		}
+		
+	}
+}
