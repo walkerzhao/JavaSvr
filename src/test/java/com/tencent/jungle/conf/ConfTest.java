ece46195505e79121deefe4c9acfd808043dfb18
andy <416548283@qq.com>
2018-08-21 22:50:06 +0800

    add test

--- src/test/java/com/tencent/jungle/conf/ConfTest.java
+++ src/test/java/com/tencent/jungle/conf/ConfTest.java
@@ -1,4 +1,4 @@
-package com.tencent.jungle.svrcore.Bootstrap;
+package com.tencent.jungle.conf;
 
 import com.google.inject.Guice;
 import com.google.inject.Injector;
@@ -7,21 +7,29 @@ import com.tencent.jungle.svrcore.comm.JungleServerCoreModule;
 import org.apache.commons.configuration.Configuration;
 import org.apache.commons.configuration.ConfigurationException;
 import org.apache.commons.configuration.PropertiesConfiguration;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
+import org.junit.Test;
+
 import java.util.ArrayList;
 import java.util.List;
 
-public class Bootstrap {
-    static Logger logger = LoggerFactory.getLogger("Console");
-    public static void main(String[] args) throws ConfigurationException {
+/**
+ * Created by andy on 2018/8/21.
+ */
+public class ConfTest {
 
+    @Test
+    public void test() throws ConfigurationException {
+        Configuration configuration = new PropertiesConfiguration("jungle.properties");
+        String value = configuration.getString("jungle.test");
+        System.out.println("hello,world." + value);
+    }
+
+    @Test
+    public void testInjectConf() {
         Injector injector = Guice.createInjector(loadModules());
         Configuration configs = injector.getInstance(Configuration.class);
-
-        // workService --> 目前使用ThreadPoolWorkerService
-        new TcpSppServer(injector, configs);
-
+        String value1 = configs.getString("jungle.test");
+        System.out.println(value1);
     }
 
     private static List<Module> loadModules() {
